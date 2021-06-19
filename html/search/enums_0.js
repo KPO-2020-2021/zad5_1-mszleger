6ae1c294cb00379c83287506615dc9d7a669f419
Michał Szleger <259235@student.pwr.edu.pl>
2021-06-19 19:30:39 +0200

    Add documentation.

---
+++ html/search/enums_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['optiontype',['optionType',['../doctest_8cpp.html#aaed2decf69a83d49ca18da9626b8f1db',1,'doctest::@30']]],
+  ['os',['Os',['../_macierz_obrotu_8hh.html#a8153ed41d0fa202555263494e01bcc19',1,'MacierzObrotu.hh']]]
+];
